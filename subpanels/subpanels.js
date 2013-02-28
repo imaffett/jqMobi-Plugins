8ce4937f7a90e484d79174f78e500af48698a2ff
Ian Maffett <ian@appmobi.com>
2013-02-28 16:03:46 -0500

    native scrolling/pull to refresh fixes

--- subpanels/subpanels.js
+++ subpanels/subpanels.js
@@ -47,25 +47,25 @@
 
   var subpanel = function(item) {
       this.container = $(item);
+
       //let's delegate the clicks to handle routing
       var self = this;
-      this.container.css("overflow",$.feat.nativeTouchScroll?"auto":"hidden");
       this.container.parent().find(".subpanelNav").on("click", "a", function(e) {
         self.loadNewDiv($(this.hash).get(), $(this).data("transition"), false);
         e.preventDefault();
       });
+      this.container.css("overflow", "hidden");
       this.container.parent().find(".subpanelNav a").data("ignore", "true"); //Set data ignore so the main click handler doesn't process it
       this.container.find("div.insetPanel").forEach(function(obj) {
         if ($(obj).attr("scrolling") == "no") return;
-        
         scrollers[obj.id] = $(obj).scroller({
           scrollBars: true,
           verticalScroll: true,
           vScrollCSS: "jqmScrollbar",
           useJsScroll: !$.feat.nativeTouchScroll,
-          autoEnable:false,
-          noParent:true
+          noParent:$.feat.nativeTouchScroll
         });
+        scrollers[obj.id].disable();
       });
       this.loadCurrent();
       this.container.bind("destroy", function() {
@@ -81,7 +81,7 @@
           var el = that.history.pop();
           that.loadNewDiv(el.old, el.trans, true, el.new);
         }
-      });
+      })
     };
 
   subpanel.prototype = {
@@ -89,18 +89,13 @@
     currentDiv: null,
     history: [],
     loadCurrent: function() {
-      try{
       var div = this.container.find(".default");
       div.css("-webkit-transform", "none");
       div.show();
       this.currentDiv = div.get();
       if (scrollers[this.currentDiv.id]) scrollers[this.currentDiv.id].enable();
-      }
-      catch(e){console.log ("error with load current "+e.message);}
-
     },
     loadNewDiv: function(newDiv, trans, back, prevDiv) {
-       try{
       if (newDiv == this.currentDiv) return;
       back = back || false;
       var oldDiv = prevDiv || this.currentDiv;
@@ -115,8 +110,6 @@
         'new': newDiv,
         'trans': trans
       });
-      }
-        catch(e){console.log ("error with load current "+e.message);}
     }
 
   };
